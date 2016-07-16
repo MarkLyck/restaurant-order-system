c7a233ec6cd05794c8e345d6c99a453a649961e3
Mark Lyck <mark.lyck@gmail.com>
2016-07-16 18:15:29 -0500

    Improved confirmation page

--- app/scripts/views/confirmationView.js
+++ app/scripts/views/confirmationView.js
@@ -5,17 +5,41 @@ import router from '../router'
 
 function renderConfirmation() {
   console.log(sessionOrder);
+  let orderNumber = sessionOrder.get('_id')
+  let time = sessionOrder.get('timeStamp')
   let $confirmationView = $(`
     <div class="modal-container">
       <div class="confirmation-modal modal">
-        <h3>Success</h3>
-        <p id="order-number">Order number: ${sessionOrder.get('_id')}</p>
-        <p id="order-time">${sessionOrder.get('timeStamp')}</p>
-        <p>Your order is being prepared!</p>
+        <h3 class="success-header">Success</h3>
+        <div class="wrapper">
+          <p id="order-number">Order number: ${orderNumber.substring(0,10)}</p>
+          <p id="order-time">Ordered on: ${moment(time).format('HH:MM MMM DD YYYY')}</p>
+        </div>
+        <ul class="ordered-items">
+
+        </ul>
         <button id="return-to-menu">Return to menu</button>
       </div>
     </div>
   `)
+  $confirmationView.filter('.modal-container').on('click', (e) => {
+    if ($(e.target)[0] === $confirmationView.filter('.modal-container')[0]) {
+      router.navigate('menu', {trigger: true})
+    }
+  })
+
+  sessionOrder.get('items').forEach(item => {
+    let $li = $(`
+      <li>
+        <h3>${item.item}</h3>
+        <p>${item.price}</p>
+      </li>
+      `)
+    // console.log(item);
+    $confirmationView.find('.ordered-items').append($li)
+  })
+
+
   $confirmationView.find('#return-to-menu').on('click', () => {
     router.navigate('menu',{trigger: true})
   })
